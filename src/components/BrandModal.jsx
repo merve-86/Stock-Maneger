49fdb18b10ee42929a609d70a4200385a3655563
merve <merveaydin86@hotmail.com>
2024-05-13 09:46:22 +0300

    Brand

---
+++ src/components/BrandModal.jsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import useStockRequest from "../services/useStockRequest";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BrandModal({ handleClose, open, info, setInfo }) {
+  const { postStock, putStock } = useStockRequest();
+
+  const handleChange = (e) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (info._id) {
+      putStock("brands", info);
+    } else {
+      postStock("brands", info);
+    }
+
+    handleClose();
+  };
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Box
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            component={"form"}
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              label="Brand Name"
+              name="name"
+              id="name"
+              type="text"
+              variant="outlined"
+              value={info.name}
+              onChange={handleChange}
+            />
+
+            <TextField
+              label="Image"
+              name="image"
+              id="image"
+              type="url"
+              variant="outlined"
+              value={info.image}
+              onChange={handleChange}
+            />
+
+            <Button variant="contained" type="submit">
+              {info._id ? "UPDATE BRAND" : "ADD BRAND"}
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
